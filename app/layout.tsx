9c8cdf3c19371f6b3e600dafceca2d0e3f81330c
rushjs1 <rushjsdev@gmail.com>
2026-04-28 16:29:22 -0400

    feat: trigger redeploy

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,9 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
+
 import "./index.css";
+
+console.log("v1");
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
